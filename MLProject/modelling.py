adb5efc3084b27d75cc1fc4a42372d110d58204c
Bigg2003 <dwifajar.2024@student.uny.ac.id>
2025-12-20 06:24:07 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
+
+
+target = "PowerConsumption_Mean"
+
+X_train = train_df.drop(columns=[target])
+y_train = train_df[target]
+
+X_test = test_df.drop(columns=[target])
+y_test = test_df[target]
+
+mlflow.set_experiment("Power Consumption Regression")
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = RandomForestRegressor(
+        n_estimators=200,
+        random_state=42,
+        n_jobs=-1
+    )
+
+    model.fit(X_train, y_train)
+
+    mlflow.sklearn.log_model(model, artifact_path="model")
+
+    y_pred = model.predict(X_test)
+
+    rmse = mean_squared_error(y_test, y_pred) ** 0.5
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print("RMSE:", rmse)
+    print("MAE :", mae)
+    print("R2  :", r2)
